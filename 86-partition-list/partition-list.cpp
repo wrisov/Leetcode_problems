1ff1de3a8ecaa1e4185eeb4c5c616109177fee29
Wrisov Karmakar <43732836+wrisov@users.noreply.github.com>
2023-08-09 01:45:32 +0530

    Time: 8 ms (39.79%) | Memory: 10.4 MB (20.01%) - LeetSync

--- 86-partition-list/partition-list.cpp
+++ 86-partition-list/partition-list.cpp
@@ -1,29 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
-    
     ListNode* partition(ListNode* head, int x) {
-        
-        ListNode* small = new ListNode(-1);
-        ListNode* large = new ListNode(-1);
-        ListNode* small_head = small;
-        ListNode* large_head = large;
-        
-        while (head){
-            if (head->val < x){
-                small->next = head;
-                small = small -> next;
-                head = head -> next;
-                small->next = NULL;
+        ListNode* store = new ListNode(0);
+        ListNode* temp = store;
+        ListNode* dummy = new ListNode(0);
+        ListNode* curr = dummy;
+        while(head!=nullptr){
+            if(head->val<x){
+                temp->next = new ListNode(head->val);
+                temp=temp->next;
+            } else {
+                curr->next = new ListNode(head->val);
+                curr=curr->next;
             }
-            else{
-                large->next = head;
-                large = large -> next;
-                head = head -> next;
-                large -> next = NULL;
-            }
+            head=head->next;
         }
-        small -> next = large_head -> next;
-
-        return small_head -> next;
+        temp->next = dummy->next;
+        return store->next;
     }
 };
