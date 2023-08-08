fb9f6f69f47e2bd4001a425dc52d056bceca528f
Wrisov Karmakar <43732836+wrisov@users.noreply.github.com>
2023-08-09 01:42:15 +0530

    Time: 4 ms (78.58%) | Memory: 10.2 MB (46.37%) - LeetSync

--- 86-partition-list/partition-list.cpp
+++ 86-partition-list/partition-list.cpp
@@ -1,54 +1,29 @@
 class Solution {
 public:
-    void solve(ListNode* &first, ListNode* &second, ListNode* &curr, int x){
-        while(curr != NULL){
-            if(curr->val < x){
-                first->next = curr;
-                first = curr;
+    
+    ListNode* partition(ListNode* head, int x) {
+        
+        ListNode* small = new ListNode(-1);
+        ListNode* large = new ListNode(-1);
+        ListNode* small_head = small;
+        ListNode* large_head = large;
+        
+        while (head){
+            if (head->val < x){
+                small->next = head;
+                small = small -> next;
+                head = head -> next;
+                small->next = NULL;
             }
             else{
-                second->next = curr;
-                second = curr;
+                large->next = head;
+                large = large -> next;
+                head = head -> next;
+                large -> next = NULL;
             }
-            curr = curr->next;
         }
-    }
+        small -> next = large_head -> next;
 
-    ListNode* partition(ListNode* head, int x) {
-        ListNode *smallHead = head, *largeHead = head;
-        
-        // Step 1 - Find the first small node and first large node
-        // Also calculate the position at which they occur
-        int sCount = 1, lCount = 1;
-        while(smallHead != NULL && smallHead->val >= x){
-            smallHead = smallHead->next;
-            sCount++;
-        }
-        while(largeHead != NULL && largeHead->val < x){
-            largeHead = largeHead->next;
-            lCount++;
-        }
-        if(smallHead == NULL || largeHead == NULL)   
-            return head;
-        
-        // Step 2 - Traverse the LL such that the two heads are adjacent
-        // based on which head occurs first
-        ListNode *first = smallHead, *second = largeHead, *curr;
-        if(sCount < lCount){
-            while(first->next != second)
-                first = first->next;
-            curr = second->next;
-        }
-        else{
-            while(second->next != first)
-                second = second->next;
-            curr = first->next;
-        }
-        
-        // Step 3 - Use solve() to form two separate LL and then merge them
-        solve(first, second, curr, x);
-        first->next = largeHead;
-        second->next = NULL;
-        return smallHead;
+        return small_head -> next;
     }
 };
