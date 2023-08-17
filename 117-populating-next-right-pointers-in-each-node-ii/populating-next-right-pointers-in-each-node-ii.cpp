681531d2c080fdd81570c0adf08b5201094c299b
Wrisov Karmakar <43732836+wrisov@users.noreply.github.com>
2023-08-17 12:36:05 +0530

    Time: 10 ms (84.48%) | Memory: 17.5 MB (28.13%) - LeetSync

--- 117-populating-next-right-pointers-in-each-node-ii/populating-next-right-pointers-in-each-node-ii.cpp
+++ 117-populating-next-right-pointers-in-each-node-ii/populating-next-right-pointers-in-each-node-ii.cpp
@@ -19,29 +19,27 @@ public:
 class Solution {
 public:
     Node* connect(Node* root) {
-        vector<Node *> arr;
-        arr.push_back(root);
-        if(root==NULL) return root;
-        while(arr.size()!=0){
-            for(int i=0;i<arr.size();i++){
-                if(i==arr.size()-1){
-                    arr[i]->next=NULL;
-                }
+        vector<Node*> v;
+        v.push_back(root);
+        if(root == NULL)
+        return root;
+        while(v.size()!=0){
+            for(int i=0;i<v.size();i++){
+                if(i==v.size()-1)
+                v[i]->next = NULL;
                 else
-                {
-                   arr[i]->next=arr[i+1];
-
-                }
+                v[i]->next = v[i+1];
             }
-            int n=arr.size();  // store it to prevent runtime infinite condition!
+            int n = v.size();
             for(int i=0;i<n;i++){
-             if(arr[i]->left!=NULL)   arr.push_back(arr[i]->left);
-             if(arr[i]->right!=NULL) arr.push_back(arr[i]->right);
-
+                if(v[i]->left != NULL)
+                v.push_back(v[i]->left);
+                if(v[i]->right != NULL)
+                v.push_back(v[i]->right);
             }
-            arr.erase(arr.begin(),arr.begin()+n);
+            v.erase(v.begin(),v.begin()+n);
         }
-           return root;
+            return root;
+
     }
- 
 };
