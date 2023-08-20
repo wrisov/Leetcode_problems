01ca2abd55f60dc4b4b3bebfae0a9edb0ec37a7d
Wrisov Karmakar <43732836+wrisov@users.noreply.github.com>
2023-08-20 21:31:55 +0530

    Time: 254 ms (77.20%) | Memory: 96.2 MB (79.67%) - LeetSync

--- 863-sum-of-distances-in-tree/sum-of-distances-in-tree.cpp
+++ 863-sum-of-distances-in-tree/sum-of-distances-in-tree.cpp
@@ -1,39 +1,46 @@
 class Solution {
 public:
-    vector<vector<int>> v;
-    vector<int> res, counter;
-    void dfs(int i, int p){
-        for(auto u: v[i]){
-            if(u==p){
+    vector<int> adj[30007];
+    vector<int> sub;
+    vector<int> dep;
+    vector<int> sum;
+    int n;
+    void dfs(int u, int par=-1){
+        sub[u]=1;
+        for(auto x: adj[u]){
+            if(x == par){
                 continue;
             }
-            dfs(u, i);
-            counter[i]=counter[i]+counter[u];
-            res[i]=res[i]+res[u]+counter[u];
+            dep[x] = dep[u] + 1;
+            dfs(x, u);
+            sub[u]+=sub[x];
         }
-        counter[i]++;
     }
-    void dfs2(int i, int n, int p){
-        for(auto u: v[i]){
-            if(u==p){
+    void dfs2(int u, int par=-1){
+        for(auto x:adj[u]){
+            if(x==par){
                 continue;
             }
-            res[u]=res[i]-counter[u]+n-counter[u];
-            dfs2(u, n, i);
+            sum[x]=sum[u]-sub[x]+(n-sub[x]);
+            dfs2(x,u);
         }
     }
-    vector<int> sumOfDistancesInTree(int n, vector<vector<int>>& edges) {
-        v.resize(n);
-        for(int i=0; i<n-1; i++){
-            int a=edges[i][0];
-            int b=edges[i][1];
-            v[a].push_back(b);
-            v[b].push_back(a);
+    vector<int> sumOfDistancesInTree(int N, vector<vector<int>>& edges) {
+        n=N;
+        sub.resize(n+1);
+        dep.resize(n+1);
+        sum.resize(n);
+        for(int i=0;i<edges.size();i++){
+            int a = edges[i][0];
+            int b = edges[i][1];
+            adj[a].push_back(b);
+            adj[b].push_back(a);
         }
-        res.resize(n);
-        counter.resize(n);
-        dfs(0, -1);
-        dfs2(0, n, -1);
-        return res;
+        dfs(0);
+        for(int i=0;i<n;i++){
+            sum[0]+=dep[i];
+        }
+        dfs2(0);
+        return sum;
     }
 };
