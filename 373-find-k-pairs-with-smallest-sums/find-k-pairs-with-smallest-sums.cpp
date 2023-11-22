26327df6da972aa1bf3ae4a405e1a44469120e26
Wrisov Karmakar <43732836+wrisov@users.noreply.github.com>
2023-11-23 03:17:36 +0530

    Time: 177 ms (87.84%) | Memory: 108.9 MB (86.30%) - LeetSync

--- 373-find-k-pairs-with-smallest-sums/find-k-pairs-with-smallest-sums.cpp
+++ 373-find-k-pairs-with-smallest-sums/find-k-pairs-with-smallest-sums.cpp
@@ -1,26 +1,20 @@
 class Solution {
 public:
     vector<vector<int>> kSmallestPairs(vector<int>& nums1, vector<int>& nums2, int k) {
-        auto compare = [&](const vector<int>& a, const vector<int>& b) {
-            return a[0] + a[1] > b[0] + b[1];
-        };
-        priority_queue<vector<int>, vector<vector<int>>, decltype(compare)> pq(compare);
-        vector<vector<int>> res;
-        if (nums1.empty() || nums2.empty() || k == 0) {
-            return res;
+        int n = nums1.size(), m = nums2.size();
+        priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>, greater<pair<int, pair<int, int>>>> pq;
+
+        for(int i=0; i<m && i<k; i++) pq.push({nums1[0] + nums2[i], {0, i}});
+
+        vector<vector<int>> ans;
+        while(ans.size() < k && pq.size()) {
+            auto a = pq.top(); pq.pop();
+            int i = a.second.first, j = a.second.second;
+
+            ans.push_back({nums1[i], nums2[j]});
+
+            if(i + 1 < n) pq.push({nums1[i + 1] + nums2[j], {i + 1, j}});
         }
-        for (int i = 0; i < nums1.size() && i < k; i++) {
-            pq.push({ nums1[i], nums2[0], 0 });
-        }
-        while (k-- > 0 && !pq.empty()) {
-            vector<int> cur = pq.top();
-            pq.pop();
-            res.push_back({ cur[0], cur[1] });
-            if (cur[2] == nums2.size() - 1) {
-                continue;
-            }
-            pq.push({ cur[0], nums2[cur[2] + 1], cur[2] + 1 });
-        }
-        return res;
+        return ans;
     }
 };
