5e06a76e34f777463cfd938e6614436dc3ec7d93
Wrisov Karmakar <43732836+wrisov@users.noreply.github.com>
2024-01-19 01:41:49 +0530

    Time: 16 ms (40.11%) | Memory: 11.4 MB (27.10%) - LeetSync

--- 1483-rank-teams-by-votes/rank-teams-by-votes.cpp
+++ 1483-rank-teams-by-votes/rank-teams-by-votes.cpp
@@ -2,31 +2,25 @@ class Solution {
 public:
     string rankTeams(vector<string>& votes) {
         int m = votes[0].size();
-        vector<vector<int>> mp(26, vector<int>(m,0)); // 2d vector to calc total votes for each
-        //ranking for each char
-        for(string& voter : votes){
-            for(int i = 0 ; i < voter.length() ; i++){
-                mp[voter[i]-'A'][i]++;
+        vector<vector<int>> mp(26,vector<int>(m,0));
+        for(int i=0;i<votes.size();i++){
+            string vote = votes[i];
+            for(int j=0;j<vote.length();j++){
+                mp[vote[j]-'A'][j]++; 
             }
         }
-
-        vector<pair<vector<int>, char>> table(26); //to store the rank chart against each char 
-
-        for(int i = 0 ; i < 26 ; i++){
-            table[i] = {mp[i],(i+'A')};
+        vector<pair<vector<int>,char>> table(26);
+        for(int i=0;i<26;i++){
+            table[i]={mp[i],(i+'A')};
         }
-
-        sort(table.begin(),table.end(),[](pair<vector<int>,char>& a,pair<vector<int>,char>& b){
-            //making the cmp according to the intruction in question
+        sort(table.begin(),table.end(),[](pair<vector<int>,char>& a, pair<vector<int>,char>& b){
             return (a.first > b.first) || (a.first == b.first && a.second < b.second);
         });
 
-        string ans = "";
-
-        for(int i = 0 ; i < min(26,m) ; i++){ //just to reduce a little bit tc
-            ans += table[i].second;
+        string ans ="";
+        for(int i=0;i<min(26,m);i++){
+            ans+= table[i].second;
         }
-
         return ans;
     }
 };
