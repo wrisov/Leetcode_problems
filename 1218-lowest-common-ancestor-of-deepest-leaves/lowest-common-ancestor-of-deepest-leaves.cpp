5336711b9f73a34f88f97d150c736b942e546081
Wrisov Karmakar <43732836+wrisov@users.noreply.github.com>
2025-04-04 16:39:29 +0530

    Time: 0 ms (100.00%) | Memory: 23 MB (16.94%) - LeetSync

---
+++ 1218-lowest-common-ancestor-of-deepest-leaves/lowest-common-ancestor-of-deepest-leaves.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    using info=pair<TreeNode*, TreeNode*>;
+    TreeNode* lcaDeepestLeaves(TreeNode* root) {
+        if (!root) return root;
+        info q[1000];
+        TreeNode* x2T[1001];// x -> TreeNode*
+        int par[1001]; // x -> parent->val
+        int front=0, back=0;
+        q[back++]={root, NULL};
+        int d=0;
+        int L=-1, R=-1;// leftmost, rightmost val in the same level
+        for( ; front!=back; d++){
+            int qz=back-front;
+            for(int i=0; i<qz; i++){// all nodes in the level d
+                auto [node, parent]=q[front++];
+                int x=node->val;
+                int y=(!parent)?-1:parent->val;
+                par[x]=y;
+                x2T[x]=node;
+                if (i==0) L=x;// leftmost
+                if (i==qz-1) R=x;// rightmost
+                
+                if (node->left) q[back++]={node->left, node};
+                if (node->right) q[back++]={node->right, node};
+            } 
+        }
+        if (L==R) return x2T[L];
+        int s=L, t=R;
+        while(s!=t){
+            s=par[s];
+            t=par[t];
+        }
+        return x2T[s];
+    }
+};
