a330343c01736800eda98b190c5abe94191c4ea6
Wrisov Karmakar <43732836+wrisov@users.noreply.github.com>
2026-05-07 02:07:38 +0530

    Time: 11 ms (65.70%) | Memory: 56.8 MB (37.44%) - LeetSync

---
+++ 1972-rotating-the-box/rotating-the-box.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    vector<vector<char>> rotateTheBox(vector<vector<char>>& boxGrid) {
+        int m = boxGrid.size();
+        int n = boxGrid[0].size();
+
+        // Step 1: simulate gravity (stones fall right)
+        for (int i = 0; i < m; i++) {
+            int empty = n - 1;
+
+            for (int j = n - 1; j >= 0; j--) {
+
+                if (boxGrid[i][j] == '*') {
+                    empty = j - 1;
+                }
+
+                else if (boxGrid[i][j] == '#') {
+                    swap(boxGrid[i][j], boxGrid[i][empty]);
+                    empty--;
+                }
+            }
+        }
+
+        // Step 2: rotate 90 degrees clockwise
+        vector<vector<char>> res(n, vector<char>(m));
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                res[j][m - 1 - i] = boxGrid[i][j];
+            }
+        }
+
+        return res;
+    }
+};
