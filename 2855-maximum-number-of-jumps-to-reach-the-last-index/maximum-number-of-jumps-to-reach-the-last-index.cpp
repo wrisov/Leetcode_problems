9a5a83abd277fe329d4c6e2064e3568bcd95fda6
Wrisov Karmakar <43732836+wrisov@users.noreply.github.com>
2026-05-11 02:21:23 +0530

    Time: 15 ms (80.86%) | Memory: 68.6 MB (41.05%) - LeetSync

---
+++ 2855-maximum-number-of-jumps-to-reach-the-last-index/maximum-number-of-jumps-to-reach-the-last-index.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int maximumJumps(vector<int>& nums, int target) {
+        int n = nums.size();
+
+        vector<int> dp(n, -1);
+        dp[0] = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (dp[i] == -1) continue;
+
+            for (int j = i + 1; j < n; j++) {
+                int diff = nums[j] - nums[i];
+
+                if (diff >= -target && diff <= target) {
+                    dp[j] = max(dp[j], dp[i] + 1);
+                }
+            }
+        }
+
+        return dp[n - 1];
+    }
+};
